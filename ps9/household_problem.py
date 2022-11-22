bug identified. policies slide downwards due to numerical imprecision

ps9/household_problem.py
@@ -3,6 +3,7 @@ import numpy as np
 from numba import jit, prange
 import numba as nb
 from market_class import assets_from_state, is_employed
+import ipdb
 
 def solve_hh(P, rate, wage, tax, labor, mu, risk_aver, disc_factor, delta, state_grid, asset_states):
 	''' Solve the HH problem
@@ -10,16 +11,20 @@ def solve_hh(P, rate, wage, tax, labor, mu, risk_aver, disc_factor, delta, state
 	Comes very close to quantecon's  policy iteration values. Sometimes the policy is slightly too high relative to quantecon (never too low). Most often the same. 
 	# Iterate on values. Return index. 
 	'''
-	
 	policy_guess = np.asarray([asset_states, asset_states])
-	tol = 1e-3
+	tol = 1e-13 # 1e-3 Smaller than minimum distance on grid
+	
 	diff = 100
+	iteration = 0
 	while diff > tol:
-		diff = 0
-		policy_guess_upd = egm_update(policy_guess, P, rate, wage, tax, labor, mu, risk_aver, disc_factor, delta, state_grid, asset_states)
-		diff = np.max(np.abs(policy_guess_upd - policy_guess))
-		policy_guess = policy_guess_upd.copy()
+		iteration += 1
+		policy_guess_upd = egm_update(policy_guess, P, rate, rate, wage, wage, tax, labor, mu, risk_aver, disc_factor, delta, state_grid, asset_states)
 
+		if iteration % 25 == 0:
+			diff = np.max(np.abs(policy_guess_upd - policy_guess))
+		
+		policy_guess = policy_guess_upd.copy()
+	
 	policy = policy_guess.flatten()
 	return(policy)
 
@@ -66,18 +71,18 @@ def mu_cons(consumption, risk_aver):
 	return(consumption**(-risk_aver))
 
 @jit(nopython=True, parallel = False)
-def egm_update(policy_guess, P, rate, wage, tax, labor, mu, risk_aver, disc_factor, delta, state_grid, asset_states):
+def egm_update(policy_guess, P, rate, rate_fut, wage, wage_fut, tax, labor, mu, risk_aver, disc_factor, delta, state_grid, asset_states):
 	''' Use EGM to solve the HH problem.
 
-	policy_guess should be values, not indices
+	policy_guess should be values, not indices, in matrix format, not vector
 
 	'''
 	# From Utility to Marginal Utility with CRRA preferences:
 	#mu_cons = ((1 - risk_aver) * R - 1)**(-risk_aver / (1 - risk_aver))
 	#mu_cons = (savings + assets*(1+r-delta) + income)**(-risk_aver)
-	exog_grid = asset_states
 	
-	policy_mat = np.empty(policy_guess.shape) 
+	policy_mat = np.empty(policy_guess.shape)
+	grid_density = asset_states[1] - asset_states[0]
 
 	#for state in range(len(state_grid)):
 	for employed in [0,1]:
@@ -91,24 +96,31 @@ def egm_update(policy_guess, P, rate, wage, tax, labor, mu, risk_aver, disc_fact
 			for state_next in range(len(state_grid)):
 				# Test whether state_next is reached by action taken before proceeding:
 				assets_fut = assets_from_state(state_next, asset_states)
-				if assets_fut != savings:
+				if np.abs(assets_fut - savings) > grid_density:
 					continue
 				assets_fut_ix = np.argmin(np.abs(assets_fut - asset_states))
 				employed_fut = is_employed(state_next, state_grid)
 
 				savings_fut = policy_guess[int(employed_fut), assets_fut_ix]
-				income_fut = wage * ((1-tax) * labor * employed_fut + mu * (1 - employed_fut))
-				consumption_fut = income_fut + assets_fut * (1+ rate - delta)- savings_fut
+				income_fut = wage_fut * ((1-tax) * labor * employed_fut + mu * (1 - employed_fut))
+				consumption_fut = income_fut + assets_fut * (1+ rate_fut - delta)- savings_fut 
 
 				mu_cons_fut = mu_cons(consumption_fut, risk_aver)
 				prob = P[int(employed), int(employed_fut)]
-				E_term += prob * mu_cons_fut * (1 + rate - delta)
+				E_term += prob * mu_cons_fut * (1 + rate_fut - delta) 
 			
 			consumption_today = (disc_factor * E_term)**(-1/risk_aver) # Rate already in expectation
 			# Mapping from actions to assets_endog, given state:
 			endog_assets.append((1/(1+rate-delta)) * (consumption_today + savings - income))
 
 		endog_assets = np.asarray(endog_assets)
-		policy_mat[employed,:] = np.interp(x = exog_grid, xp = endog_assets, fp = asset_states)
+		policy_mat[employed,:] = np.interp(x = asset_states, xp = endog_assets, fp = asset_states)
 
 	return(policy_mat)
+
+
+
+
+
+
+

ps9/incomplete_market.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-from household_problem import solve_hh, policy_to_grid
+from household_problem import solve_hh, policy_to_grid, egm_update
 from distribution_funcs import get_transition_matrix, get_distribution_fast
 from mit_shock import solve_trans_path
 from market_class import Market
@@ -19,6 +19,7 @@ from scipy import optimize
 import scipy as sp
 from scipy import linalg
 from scipy.interpolate import griddata
+
 
 
 import warnings
@@ -80,13 +81,13 @@ steady_state = Market(
 	sigma = 0.007,
 	a_size = 1000) # 1000
 
-#sol = minimize_scalar(objective_function, bounds=(0.01, 0.04), method='bounded', args = steady_state)#, options={'xatol': 1e-12, 'maxiter': 500, 'disp': 0})
+sol = minimize_scalar(objective_function, bounds=(0.01, 0.04), method='bounded', args = steady_state)#, options={'xatol': 1e-12, 'maxiter': 500, 'disp': 0})
 
-#rate_ss = sol.x
-rate_ss = 0.022590438230091416
+rate_ss = sol.x
+#rate_ss = 0.022589966512929882
 print("The interest rate is:")
 print(sol.x)
-
+rate_ss = sol.x
 #objective_function(sol.x, steady_state)
 
 #rate_ss = 0.034372948575014266 
@@ -95,26 +96,23 @@ print(sol.x)
 steady_state.set_prices(rate_ss)
 steady_state.K
 
-policy = solve_hh(steady_state.P, rate_ss, steady_state.wage, steady_state.tax, steady_state.L_tilde, steady_state.mu, steady_state.gamma, steady_state.beta,steady_state.delta, steady_state.state_grid, steady_state.asset_states)
+policy_ss = solve_hh(steady_state.P, rate_ss, steady_state.wage, steady_state.tax, steady_state.L_tilde, steady_state.mu, steady_state.gamma, steady_state.beta,steady_state.delta, steady_state.state_grid, steady_state.asset_states)
 
-policy_ix_up, alpha_list = policy_to_grid(policy, steady_state.asset_states)
-policy_ss = policy_ix_up
+policy_ix_up, alpha_list = policy_to_grid(policy_ss, steady_state.asset_states) 
+
 P_ss = get_transition_matrix(steady_state.Q, nb.typed.List(policy_ix_up), nb.typed.List(alpha_list), steady_state.state_grid)
 P_ss = np.asarray(P_ss)
-
-
 distr_guess = np.full(steady_state.state_grid.shape, 1)/len(steady_state.state_grid)
 distr = get_distribution_fast(distr_guess, P_ss, steady_state.state_grid)
 
-objective_function(rate_ss, steady_state)
-
 ss = steady_state
-T = 500
-K_guess = np.repeat(ss.K, T+1)
-policy_ss = np.reshape(np.asarray(policy_ss), (2, 1000))
-policy_ss = ss.asset_states[policy_ss]
 
-K_sol, K_HH_evol, tfp_seq, T = solve_trans_path(ss, T, distr, policy_ss, K_guess)
+#ss.set_tfp(1, 75.63617405185936)
+T = 400
+K_guess = np.repeat(ss.K, T+1)
+
+policy_ss = np.reshape(np.asarray(policy_ss), (2, 1000), order = "C")
+K_sol, K_HH_evol, tfp_seq, T, rate_path, wage_path = solve_trans_path(ss, T, distr, policy_ss, K_guess)
 
 
 K_sol.shape
@@ -159,5 +157,10 @@ fig.show()
 
 	tmp = np.linspace(0,10,10)
 	np.insert(tmp, 0, 1000)
+
+get_distribution(P)
+
+
+
 
 

ps9/market_class.py
@@ -1,4 +1,4 @@
-
+import quantecon as qe
 import numpy as np
 from numba import jit, prange
 
@@ -20,6 +20,7 @@ class	Market:
 		rho = 0.95,
 		sigma = 0.007,
 		a_size = 1000):
+		mc = qe.MarkovChain(P)
 		self.r = r
 		self.hh_panel = hh_panel
 		self.gamma = gamma # Risk aversion
@@ -32,10 +33,10 @@ class	Market:
 		self.sigma = sigma # TODO: what does he mean?
 		self.rho = rho
 		self.T = T
-		self.unemployment_rate = 1 - np.mean(hh_panel) # series of unemployment
+		self.unemployment_rate = mc.stationary_distributions[0][0]
 		self.L_tilde = 1 / ( 1 - self.unemployment_rate) # How much they work, given that they work. Implies L = 1
 		self.L = 1
-		self.A = 1 # TODO: make general
+		self.A = 1 
 		self.capital_from_r()
 		self.s_size = 2 * a_size # Number of states (TODO employed unemployed X num assets)
 		self.a_size = a_size # number of actions
@@ -171,8 +172,6 @@ def assets_from_state(s, asset_grid):
 	state grid first has all unemployed all assets, then all employed and all assets again
 	'''
 	remainder = s % len(asset_grid)
-	if remainder == 0:
-		return(asset_grid[0])
-	assets = asset_grid[s % len(asset_grid)]
+	assets = asset_grid[remainder]
 	return(assets)
 

ps9/mit_shock.py
@@ -7,49 +7,43 @@ import ipdb
 
 # Solve for transition path to small shock (e.g. TFP) imposing market clearing
 def solve_trans_path(ss, T, distr0, policy_ss, K_guess):
-
-	#  Guess path for capital and labor
-		# K can be a linear decay
+	# TODO: policy 'slides down', so try a larger shock that offsets this effect an decrrease time horizon (less time to slide down) Tested T = 400 and 0.025 shock
 	K0 = ss.K 
 	diff = 100
 	tol = 1e-6
-	weight = 2e-1
-
-	# TODO: look at capital to output ratio. Scale by shock size for guess of capital in the shock period, then decay this
-	#K_guess = np.linspace(38.4, K, num = T+1)
-	#K_guess[0] = K
+	weight = 1e-1
 	
-	shock = 0.01
-	tfp =  1 + shock * 0.95**np.linspace(0,T, T)
+	shock = 0.025
+	tfp =  1 + shock * 0.95**np.linspace(0,T, T) 
 	tfp = np.insert(tfp, 0, 1)
 	tfp[-1] = 1
 	
-	tfp[0] = 1
 	n_iters = 0
-	#distr_guess = [np.full(ss.state_grid.shape, 1)/len(ss.state_grid)]
 
 	K_HH_list = []
 
-	while diff > tol and n_iters < 200:
+	while diff > tol and n_iters < 50:
 		n_iters += 1
+
 		# Solve the value function/household policy backwards
-		
-		policy = backward_iterate_policy(tfp, K_guess, policy_ss, T, ss)
+		policy, rate_path, wage_path = backward_iterate_policy(tfp, K_guess, policy_ss, T, ss)
 		
 		# From SS, solve forward using policy functions and idiosyncratic Markov process
 		distr = forward_iterate_distribution(policy, distr0, T, ss)
-		
 		
 		# Calculate capital supplied at each point in time
 		flat_pols = []
 		for pol in policy:
 			flat_pols.append(pol.flatten())
+
+		assert np.all(np.isclose(np.sum(np.asarray(distr), axis = 1), 1)), "Summing wrong axis"
+
 		K_HH = np.sum(np.asarray(flat_pols) * np.asarray(distr), axis = 1)
-		K_HH = K_HH[0:T] # HH savings in period t # Should correspond to assets in t+1
+		K_HH = K_HH[0:T] # HH savings in period t that should correspond to firm assets in period t+1
 
 		K_HH_list.append(K_HH)
 
-		# Check max difference # TODO: K_hh_t is distr * savings whereas k_guess is total assets. Need to compare to values in the next period
+		# Check max difference 
 		diff_array = K_HH - K_guess[1:T+1]
 		diff = np.max(diff_array)**2
 		diff_ix = np.argmax(np.abs(diff_array))
@@ -58,9 +52,9 @@ def solve_trans_path(ss, T, distr0, policy_ss, K_guess):
 		K_guess = (1- weight) * K_guess[1:T+1] + (weight) * K_HH
 		K_guess = np.insert(K_guess, 0, K0)
 		print((diff_array[diff_ix], weight))
-		weight = np.maximum(weight*0.925, 1e-5)
+		weight = np.maximum(weight*1, 1e-5)
 
-	return(K_guess, K_HH_list, tfp, T)
+	return(K_guess, K_HH_list, tfp, T, rate_path, wage_path)
 
 
 def backward_iterate_policy(tfp, K_guess, policy_ss, T, ss):
@@ -69,20 +63,33 @@ def backward_iterate_policy(tfp, K_guess, policy_ss, T, ss):
 	policy_ss : is the policy in steady state. A numpy array of income states X asset states containing values (not indices) of savings. 
 	'''
 	policy_list = [policy_ss]
+	rate_fut = ss.r 
+	wage_fut = ss.wage
+	rate_path = [rate_fut]
+	wage_path = [wage_fut]
 	for time in reversed(range(T)):
-		tfp_t = tfp[time]
+		
+		tfp_t = tfp[time] # First 'time' is T-1
 		K_t = K_guess[time]
 		ss.set_tfp(tfp_t, K_t)
+		rate = ss.r
+		wage = ss.wage
 		
 		policy_prev = policy_list[-1]
-		
-		policy_t = egm_update(policy_prev, ss.P, ss.r, ss.wage, ss.tax, ss.L_tilde, ss.mu, ss.gamma, ss.beta, ss.delta, ss.state_grid, ss.asset_states) # Can use egm_update, do not iterate on it
+		# TODO: it seems egm_update is not at a fixed point. policy_prev != policy_t when all other input are as in steady state
+		policy_t = egm_update(policy_prev, ss.P, rate, rate_fut, wage, wage_fut, ss.tax, ss.L_tilde, ss.mu, ss.gamma, ss.beta, ss.delta, ss.state_grid, ss.asset_states) # Can use egm_update, do not iterate on it
 		policy_list.append(policy_t)
+		rate_fut = np.copy(rate)
+		wage_fut = np.copy(wage)
+		rate_path.append(rate_fut)
+		wage_path.append(wage_fut)
 	
 	policy_list.reverse()
+	rate_path.reverse()
+	wage_path.reverse()
 	policy = np.asarray(policy_list)
 	
-	return(policy)
+	return(policy, rate_path, wage_path)
 
 
 def forward_iterate_distribution(policy, distr0, T, ss):
@@ -98,10 +105,11 @@ def forward_iterate_distribution(policy, distr0, T, ss):
 		policy_t_ix, alpha_list = policy_to_grid(policy_t.flatten(), ss.asset_states)
 		P = get_transition_matrix(ss.Q, nb.typed.List(policy_t_ix), nb.typed.List(alpha_list), ss.state_grid) 
 		P = np.asarray(P)
-		distr_t = get_distribution_fast(distr0, P, ss.state_grid, tol = 1e-10) # TODO better initial guess
+		distr_t = get_distribution_fast(distr_list[-1], P, ss.state_grid, tol = 1e-10)
 	
 		distr_list.append(distr_t)
 	distr = np.asarray(distr_list)
 	return(distr)
+
 
 